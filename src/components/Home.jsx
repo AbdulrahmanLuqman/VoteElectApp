d74e115bbda2c05e55c1a232f06f00f51f00e607
antimonycodes <akinrinadetobilobasb@gmail.com>
2024-05-09 21:26:08 +0100

    Add @tanstack/react-query; update SignIn.jsx to handle new sign-in response format
    * Add @tanstack/react-query (^5.35.1) to package dependencies and lockfile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,25 +3,25 @@
 import { MdLocationPin } from "react-icons/md";
 import { IoMdNotifications } from "react-icons/io";
 import { Link } from "react-router-dom";
-import wavingFlags from "../assets/images/group-of-people-waving-flags.jpg"
-import BottomNavLinks from "./BottomNavLinks"
+import wavingFlags from "../assets/images/group-of-people-waving-flags.jpg";
+import BottomNavLinks from "./BottomNavLinks";
 
 const Home = () => {
   // const = [{
   //     // id:1,fullName:""
   // }]
   return (
-    <div className=" bg-green-100 h-screen ">
+    <div className="h-screen bg-green-100 ">
       {/* TOP */}
-      <div className=" flex items-center justify-between py-6  px-4">
-        <div className=" flex gap-4 ">
+      <div className="flex items-center justify-between px-4 py-6 ">
+        <div className="flex gap-4 ">
           <div>
             <img
               src="/flower2.jpg"
               alt="profile-pics"
               //   width={60}
               //   height={60}
-              className=" w-14 h-14 rounded-full"
+              className="rounded-full w-14 h-14"
             />
           </div>
           {/* location */}
@@ -29,25 +29,34 @@ const Home = () => {
             <div>
               <h1>Hello, Username</h1>
             </div>
-            <div className=" flex items-center  ">
-              <MdLocationPin className=" text-green-500" />
-              <h1 className=" text-green-500">Lagos, Nigeria.</h1>
+            <div className="flex items-center ">
+              <MdLocationPin className="text-green-500 " />
+              <h1 className="text-green-500 ">Lagos, Nigeria.</h1>
             </div>
           </div>
         </div>
         <Link to="/notifications">
-          <IoMdNotifications className=" text-black text-3xl " />
+          <IoMdNotifications className="text-3xl text-black " />
         </Link>
       </div>
       <div className="relative h-[150px]">
-        <img className="absolute h-full w-full top-0" src={wavingFlags} alt="group of people waving flags" />
-        <div className="absolute top-0 bg-black opacity-50 h-full w-full"></div>
-        <div className="absolute top-0 h-full w-full flex items-center justify-center">
-          <p className="text-white text-[1.5rem] font-bold">EXERCISE YOUR RIGHT TO VOTE</p>
+        <img
+          className="absolute top-0 w-full h-full"
+          src={wavingFlags}
+          alt="group of people waving flags"
+        />
+        <div className="absolute top-0 w-full h-full bg-black opacity-50"></div>
+        <div className="absolute top-0 flex items-center justify-center w-full h-full">
+          <p className="text-white text-[1.5rem] font-bold">
+            EXERCISE YOUR RIGHT TO VOTE
+          </p>
         </div>
       </div>
       <div className="w-[90%] bg-white text-center p-4 rounded-md shadow-md mx-auto relative bottom-8">
-        <p className="text-[1.1rem] font-semibold">See <Link className="text-green-500">Ongoing</Link> or <Link className="text-green-500">Upcoming</Link> Election</p>
+        <p className="text-[1.1rem] font-semibold">
+          See <Link className="text-green-500">Ongoing</Link> or{" "}
+          <Link className="text-green-500">Upcoming</Link> Election
+        </p>
       </div>
       <div className="w-[90%] bg-white p-4 rounded-md shadow-md mx-auto">
         <div className="flex items-center justify-between">
@@ -57,24 +66,40 @@ const Home = () => {
           </div>
           <div className="flex gap-2">
             <div className="flex flex-col items-center">
-              <img class="rounded-[50%]" src="https://via.placeholder.com/50" />
+              <img
+                className="rounded-[50%]"
+                src="https://via.placeholder.com/50"
+              />
               <p className="text-gray-400">45%</p>
             </div>
             <div className="flex flex-col items-center">
-              <img class="rounded-[50%]" src="https://via.placeholder.com/50" />
+              <img
+                className="rounded-[50%]"
+                src="https://via.placeholder.com/50"
+              />
               <p className="text-gray-400">25%</p>
             </div>
           </div>
         </div>
       </div>
       {/* News */}
-      <div className="space-y-2 p-4">
+      <div className="p-4 space-y-2">
         <h2 className="text-green-500 text-[1.1rem]">Trending New</h2>
         <div className="flex gap-4">
-          <img width={100} height={100} class="rounded-xl h-full" src="https://via.placeholder.com/50" />
+          <img
+            width={100}
+            height={100}
+            className="h-full rounded-xl"
+            src="https://via.placeholder.com/50"
+          />
           <div>
-            <h2 className="font-bold text-[1.1rem]">Voting Unit in Ilorin has stopped</h2>
-            <p className="text-[12px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, iure.</p>
+            <h2 className="font-bold text-[1.1rem]">
+              Voting Unit in Ilorin has stopped
+            </h2>
+            <p className="text-[12px]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+              iure.
+            </p>
           </div>
         </div>
       </div>
